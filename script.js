fa3dd4b34c793fce3cba23386befdcfa53119827
feha320 <104710422+feha320@users.noreply.github.com>
2023-07-19 20:16:24 +0200

    added remove function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
   databaseURL: "https://shopping-list-2e023-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -12,6 +12,7 @@ const shoppingItemsInDB = ref(database, "shoppingItems");
 const mainButtonEl = document.getElementById("main-button");
 const mainInputEl = document.getElementById("main-input");
 const shoppingListEl = document.getElementById("shopping-list");
+const shoppingListCompletedEl = document.getElementById("shopping-list-completed");
 
 mainButtonEl.addEventListener("click", function() {
   let inputValue = mainInputEl.value;
@@ -44,12 +45,28 @@ function appendItem(item) {
   let itemValue = item[1];
   let newEl = document.createElement("li");
   newEl.classList.add("basic-style");
+  newEl.addEventListener("click", function() {
+    let clickedID = ref(database, `shoppingItems/${itemID}`);
+    moveItem(item);
+    remove(clickedID);
+  });
   newEl.textContent = itemValue;
   shoppingListEl.append(newEl);
-
 }
 
+function moveItem(item) {
+  
+  let itemID = item[0];
+  let itemValue = item[1];
+  let newEl = document.createElement("li");
+  newEl.classList.add("basic-style", "completed");
+  newEl.addEventListener("click", function() {
+    newEl.remove();
+  })
+  newEl.textContent = itemValue;
+  shoppingListCompletedEl.append(newEl);
+}
 
 function clearItems() {
-  shoppingListEl.innerHTML = ""
+  shoppingListEl.innerHTML = "";
 }
