28a889c2dd71db28f83d2c97b476b4ffc5aacdc1
feha320 <104710422+feha320@users.noreply.github.com>
2023-07-21 19:40:24 +0200

    added message if shopping list is empty

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-  databaseURL: "https://shopping-list-2e023-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+  databaseURL:
+    "https://shopping-list-2e023-default-rtdb.europe-west1.firebasedatabase.app/",
+};
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
@@ -12,40 +19,45 @@ const shoppingItemsInDB = ref(database, "shoppingItems");
 const mainButtonEl = document.getElementById("main-button");
 const mainInputEl = document.getElementById("main-input");
 const shoppingListEl = document.getElementById("shopping-list");
-const shoppingListCompletedEl = document.getElementById("shopping-list-completed");
+const shoppingListCompletedEl = document.getElementById(
+  "shopping-list-completed"
+);
 
-mainButtonEl.addEventListener("click", function() {
+mainButtonEl.addEventListener("click", function () {
   let inputValue = mainInputEl.value;
   clearMainInputEl();
   appendItem(inputValue);
   console.log(inputValue);
   push(shoppingItemsInDB, inputValue);
-})
+});
 
-onValue(shoppingItemsInDB, function(snapshot) {
-  let itemsArray = Object.entries(snapshot.val());
-  
-  clearItems();
-  
-  for (let i = 0; i < itemsArray.length; i++) {
-    let currentItem = itemsArray[i];
-    let currentItemID = currentItem[0];
-    let currentItemValue = currentItem[1];
-    appendItem(currentItem);
+onValue(shoppingItemsInDB, function (snapshot) {
+  if (snapshot.exists()) {
+    let itemsArray = Object.entries(snapshot.val());
+
+    clearItems();
+
+    for (let i = 0; i < itemsArray.length; i++) {
+      let currentItem = itemsArray[i];
+      let currentItemID = currentItem[0];
+      let currentItemValue = currentItem[1];
+      appendItem(currentItem);
+    }
+  } else {
+    shoppingListEl.innerHTML = "Einkaufszettel leer!";
   }
-})
+});
 
 function clearMainInputEl() {
   mainInputEl.value = "";
 }
 
 function appendItem(item) {
-  
   let itemID = item[0];
   let itemValue = item[1];
   let newEl = document.createElement("li");
   newEl.classList.add("basic-style");
-  newEl.addEventListener("click", function() {
+  newEl.addEventListener("click", function () {
     let clickedID = ref(database, `shoppingItems/${itemID}`);
     moveItem(item);
     remove(clickedID);
@@ -55,14 +67,13 @@ function appendItem(item) {
 }
 
 function moveItem(item) {
-  
   let itemID = item[0];
   let itemValue = item[1];
   let newEl = document.createElement("li");
   newEl.classList.add("basic-style", "completed");
-  newEl.addEventListener("click", function() {
+  newEl.addEventListener("click", function () {
     newEl.remove();
-  })
+  });
   newEl.textContent = itemValue;
   shoppingListCompletedEl.append(newEl);
 }
